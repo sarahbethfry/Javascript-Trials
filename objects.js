c738e15982815ec12a4f1d03666e498d767a1fd9
Your Name <you@example.com>
2020-01-30 17:56:10 -0800

    javascript

--- objects.js
+++ objects.js
@@ -1,13 +1,33 @@
 "use strict";
-
+const str = 'The quick brown fox jumps over the lazy dog.';
 
 // 1. countWords
 function countWords(phrase) {
   // Replace this with your code
+  const word_counts = {};
+
+  for(const word of phrase.split(' ')){
+    if (word_counts.hasOwnProperty(word)){
+      word_counts[word] += 1;
+    }else{
+      word_counts[word] = 1;
+    }
+  }
+  return word_counts;
 }
 
 
 // 2. getMelonsAtPrice
 function getMelonsAtPrice(price) {
-  // Replace this with your code
+  const melonsPrices = {
+    2.50: ['Cantaloupe', 'Honeydew'],
+    2.95: ['Watermelon'],
+    3.25: ['Musk', 'Crenshaw'],
+    14.25: ['Christmas']
+  }
+
+  if (melonsPrices.hasOwnProperty(price)){
+    return melonsPrices[price];
+
+  } 
 }
